3aff63795deb9ab86510a971f3f2945466bb6c66
storemalt <storemalt@gmail.com>
2015-03-30 12:56:30 +0800

    - Add Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,14 +15,14 @@ Route::get('/',                 'WelcomeController@index');
 Route::get('home',              'HomeController@index');
 
 // course routes
-Route::get('courses',           'CourseController@index');
-Route::get('course/create',     'CourseController@create');
-Route::post('course/create',    'CourseController@store');
-Route::get('course/edit/{id}',  'CourseController@edit')->where('id','\d+');
-Route::post('course/edit/', 	'CourseController@update');
-Route::get('course/view/{id}',  'CourseController@show')->where('id','\d+');
-Route::get('course/destroy/{id}',  'CourseController@destroy')->where('id','\d+');
-Route::get('course/page/{id}',  'CourseController@page')->where('id','\d+');
+Route::get('courses',           	'CourseController@index');
+Route::get('course/create',     	'CourseController@create');
+Route::post('course/create',    	'CourseController@store');
+Route::get('course/edit/{id}',  	'CourseController@edit')->where('id','\d+');
+Route::post('course/edit/', 		'CourseController@update');
+Route::get('course/view/{id}',  	'CourseController@show')->where('id','\d+');
+Route::get('course/destroy/{id}',  	'CourseController@destroy')->where('id','\d+');
+Route::get('course/page/{id}',  	'CourseController@page')->where('id','\d+');
 
 // lesson routes
 Route::get('lessons',           'LessonController@index');
